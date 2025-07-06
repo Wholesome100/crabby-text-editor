efc93dc608eebca6f4ff177dbebd8655d58febd8
Wholesome100 <77867156+Wholesome100@users.noreply.github.com>
2025-07-06 11:42:28 -0400

    Basic ropey example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-fn main() {
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+use ropey::Rope;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Welcome to Crabby!
     // This is a basic text editor using clap, ropey, ratatui and crossterm
-    println!("Hello, world!");
+   let mut text = Rope::from_reader(
+    BufReader::new(File::open("src/store/dummy.txt")?)
+   )?;
+   
+   println!("{}", text.line(3));
+
+   let start_idx = text.line_to_char(3);
+   let end_idx = text.line_to_char(4);
+
+   text.remove(start_idx..end_idx);
+
+   text.insert(start_idx, "Waiting to hold some cool words");
+
+   let start_idx = text.line_to_char(3);
+   let end_idx = text.line_to_char(4);
+   println!("{}", text.slice(start_idx..end_idx));
+
+   text.write_to(
+    BufWriter::new(File::create("src/store/dummychanged.txt")?)
+   )?;
+
+    Ok(())
 }
